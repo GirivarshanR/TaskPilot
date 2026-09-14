6a84a58721f292ee84a0d2d1f9b41b144e1d1a0f
GirivarshanR <girivarshanr@gmail.com>
2026-09-15 02:05:25 +0530

    Add task creation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const tasksContainer = document.getElementById("tasksContainer");
+
+
+addTaskBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value;
+
+    if (taskText === "") {
+        return;
+    }
+
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const taskTextElement = document.createElement("span");
+    taskTextElement.textContent = taskText;
+
+    taskElement.appendChild(taskTextElement);
+    tasksContainer.appendChild(taskElement);
+
+    taskInput.value = "";
+});
