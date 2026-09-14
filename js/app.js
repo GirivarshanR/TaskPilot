ab92824f7853886da56ac77cb3a0a42499c7b816
GirivarshanR <girivarshanr@gmail.com>
2026-09-15 02:08:35 +0530

    Add task data model

--- js/app.js
+++ js/app.js
@@ -2,14 +2,10 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const tasksContainer = document.getElementById("tasksContainer");
 
+const tasks = [];
 
-addTaskBtn.addEventListener("click", function () {
 
-    const taskText = taskInput.value.trim();
-
-    if (taskText === "") {
-        return;
-    }
+function createTask(task) {
 
     const taskElement = document.createElement("div");
     taskElement.classList.add("task");
@@ -18,11 +14,13 @@ addTaskBtn.addEventListener("click", function () {
     checkbox.type = "checkbox";
 
     const taskTextElement = document.createElement("span");
-    taskTextElement.textContent = taskText;
+    taskTextElement.textContent = task.title;
 
     checkbox.addEventListener("change", function () {
 
-        taskTextElement.style.textDecoration = checkbox.checked
+        task.completed = checkbox.checked;
+
+        taskTextElement.style.textDecoration = task.completed
             ? "line-through"
             : "none";
     });
@@ -32,7 +30,16 @@ addTaskBtn.addEventListener("click", function () {
     deleteBtn.textContent = "Delete";
 
     deleteBtn.addEventListener("click", function () {
+
+        const taskIndex = tasks.findIndex(function (item) {
+            return item.id === task.id;
+        });
+
+        tasks.splice(taskIndex, 1);
+
         taskElement.remove();
+
+        console.log(tasks);
     });
 
 
@@ -41,6 +48,28 @@ addTaskBtn.addEventListener("click", function () {
     taskElement.appendChild(deleteBtn);
 
     tasksContainer.appendChild(taskElement);
+}
+
+
+addTaskBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        return;
+    }
+
+    const newTask = {
+        id: Date.now(),
+        title: taskText,
+        completed: false
+    };
+
+    tasks.push(newTask);
+
+    console.log(tasks);
+
+    createTask(newTask);
 
     taskInput.value = "";
 });
