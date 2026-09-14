9157a7f217b24014e2396f175c492df9ee4509ba
GirivarshanR <girivarshanr@gmail.com>
2026-09-15 02:06:17 +0530

    Add task completion

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const tasksContainer = document.getElementById("tasksContainer");
 
 addTaskBtn.addEventListener("click", function () {
 
-    const taskText = taskInput.value;
+    const taskText = taskInput.value.trim();
 
     if (taskText === "") {
         return;
@@ -14,10 +14,22 @@ addTaskBtn.addEventListener("click", function () {
     const taskElement = document.createElement("div");
     taskElement.classList.add("task");
 
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
     const taskTextElement = document.createElement("span");
     taskTextElement.textContent = taskText;
 
+    checkbox.addEventListener("change", function () {
+
+        taskTextElement.style.textDecoration = checkbox.checked
+            ? "line-through"
+            : "none";
+    });
+
+    taskElement.appendChild(checkbox);
     taskElement.appendChild(taskTextElement);
+
     tasksContainer.appendChild(taskElement);
 
     taskInput.value = "";
